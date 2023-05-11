19f5fe2084c2c8084efd2468b29fdd5e9ec9431c
Ivan <ivanjose_04@hotmail.com>
2023-05-11 12:00:09 -0500

    first commit

---
+++ src/paquete_test/Main.java
@@ -0,0 +1,19 @@
+package paquete_test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero = 5;
+        long numero2 = 213213;
+        double numero3 = 15.2;
+        boolean valor = true;
+        String texto = "Ivan";
+
+        System.out.println(numero);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(valor);
+        System.out.println(texto);
+    }
+}
